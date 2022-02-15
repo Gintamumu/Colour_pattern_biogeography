3232447010ee28a90b3855e3b48fc0c94d65d95b
Gintamumu <40757024+Gintamumu@users.noreply.github.com>
2022-02-15 16:09:01 +0000

    Update env_value_add_check.r

--- 03_Environmental_value_add_and_check/env_value_add_check.r
+++ 03_Environmental_value_add_and_check/env_value_add_check.r
@@ -1,10 +1,14 @@
 #### 2.Environmental_value_add_and_check ####
 
 ####Input files####
-##01 Dchrysippus_DAS_records_Auguest2021_curated_20211124_secondCheck_rm291.processed2.csv##
-##02 Martin_and_Kenya_collection_20211123_edithead2.processed2.csv##
-##03 GBIF_Dchrysippus_20201206_20210721_20211101_2_2_5644_177_research2.processed2.csv##
-##04 GBIF_Dchrysippus_20201206_20210721_20211101_2_2_5644_5467_citizen.processed_coordinate_edit1.csv####
+###processed full dataset.csv##
+###Title and content###
+###gbifID	associatedReferences	references	occurrenceID	individualCount	indNumber	lifeStage	sex	hindwingWhite	forewingTip	groundColour	sideVisible	decimalLatitude	decimalLongitude	coordinateUncertaintyInMeters	eventDate	eventTime	year	month	day	identifier	catalogNumber	countryCode	stateProvince	level0Gid	level0Name	level1Gid	level1Name	level2Gid	level2Name	level3Gid	level3Name	institutionCode	Citizen...Research	No	aa	A.	bb	B.	cc	C.	male	female	Total
+###3325960434	NA	https://www.inaturalist.org/observations/86131889	https://www.inaturalist.org/observations/86131889	1		Adult	NA	absent	present	light	upperside	22.2445	114.176449	1	2021-07-08T13:46:00	05:46:00Z	2021	7	8	189270459	86131889	HK	Southern	HKG	Hong Kong	HKG.11_1	Southern					iNaturalist	Citizen	1	0	1	1	0	1	0	NA	NA	1
+###3325954372	NA	https://www.inaturalist.org/observations/86037377	https://www.inaturalist.org/observations/86037377	1		Adult	male	absent	present	NA	underside	-33.94398	22.462978	NA	2021-07-07T09:42:00	07:42:00Z	2021	7	7	189084077	86037377	ZA	Western Cape	ZAF	South Africa	ZAF.9_1	Western Cape	ZAF.9.4_1	Eden	ZAF.9.4.2_1	George	iNaturalist	Citizen	2	0	1	NA	NA	1	0	1	0	1
+###3325926346	NA	https://www.inaturalist.org/observations/85988456	https://www.inaturalist.org/observations/85988456	1		Adult	male	present	present	light	upperside	10.824091	1.124783	NA	2021-06-29T11:19:07	09:19:07Z	2021	6	29	189027184	85988456	BJ	Atakora	BEN	Benin	BEN.2_1	Atakora	BEN.2.5_1	Mat??ri			iNaturalist	Citizen	3	1	0	1	0	1	0	1	0	1
+
+
 ####Same codes####
 
 #Load Libraries
@@ -16,26 +20,26 @@ data(wrld_simpl)
 library(maps)
 
 #setwd workplace
-setwd("/ceph/users/wliu/SP/Danaus_SDM/03_value_check/Full/research/")
+setwd("/file_location/")
 
 # variables_check
 ### Environmental Source https://www.climond.org/BioclimData.aspx
 ### Annual
-Bio01 <- raster("/ceph/users/wliu/SP/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio01_V1.2.asc")
-Bio12 <- raster("/ceph/users/wliu/SP/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio12_V1.2.asc")
-Bio20 <- raster("/ceph/users/wliu/SP/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio20_V1.2.asc")
-Bio28 <- raster("/ceph/users/wliu/SP/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio28_V1.2.asc")
+Bio01 <- raster("/file_location/CM10_1975H_Bio01_V1.2.asc")
+Bio12 <- raster("/file_location/CM10_1975H_Bio12_V1.2.asc")
+Bio20 <- raster("/file_location/CM10_1975H_Bio20_V1.2.asc")
+Bio28 <- raster("/file_location/CM10_1975H_Bio28_V1.2.asc")
 
 ### Seasonality
-Bio04 <- raster("/data/martin/genomics/analyses/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio04_V1.2.asc")
-Bio15 <- raster("/data/martin/genomics/analyses/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio15_V1.2.asc")
-Bio23 <- raster("/data/martin/genomics/analyses/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio23_V1.2.asc")
-Bio31 <- raster("/data/martin/genomics/analyses/Danaus_SDM/02_Layers/asc/01_Bio/CM10_1975H_Bio31_V1.2.asc")
+Bio04 <- raster("/file_location/CM10_1975H_Bio04_V1.2.asc")
+Bio15 <- raster("/file_location/CM10_1975H_Bio15_V1.2.asc")
+Bio23 <- raster("/file_location/CM10_1975H_Bio23_V1.2.asc")
+Bio31 <- raster("/file_location/CM10_1975H_Bio31_V1.2.asc")
 
 
 # ---------------_checkING DISTRIBUTION DATA FOR NA POINTS
-file_dir <- "/ceph/users/wliu/SP/Danaus_SDM/03_value_check/Full/research/"
-file_prefix <- "Dchrysippus_DAS_records_Auguest2021_curated_20211124_secondCheck_rm291.processed2.ValueCheck4"
+file_dir <- "/file_locations/"
+file_prefix <- "your_file"
 records <- read.csv(paste0(file_dir, file_prefix,".csv"), header = T, stringsAsFactors = F)
 
 ###Order of Coordinate is (longitude,latitude)####
