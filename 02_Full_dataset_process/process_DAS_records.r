a2d60b9d47ed6bfe8c7aa97c82e1b5e9580e8989
Gintamumu <40757024+Gintamumu@users.noreply.github.com>
2022-02-15 15:58:33 +0000

    Update process_DAS_records.r

--- 02_Full_dataset_process/process_DAS_records.r
+++ 02_Full_dataset_process/process_DAS_records.r
@@ -2,22 +2,22 @@
 
 ###01.process_DAS_records.r####
 
+###Identification of D. chrysippus genotypes at the A, B and C loci###
+###Hindwing White (A-:absent,Aa:partial,aa:present)
+###Ground Colour (B-:present/Dark,Bb:intermediate,bb:absent/light)
+###Forewing Tip (C-:absent,Cc:intermediate,cc:present)
+
 ### Input file example 
 ### Site	Sex	Total 	A-	Aa	aa	C-	Cc	cc	B-	bb	latitude	longitude	decimalLatitude	decimalLongitude	collector	dataset
 ### 1	m	23	23	0	0	0	0	23	0	23	6.06N	100.23E	6.1	100.383	dass	Smith field notes
 ### 1	f	20	20	0	0	0	0	20	0	20	6.06N	100.23E	6.1	100.383	dass	Smith field notes
-
+### Sex, m is male, f is female; Columns 3-11 are number of records for each genotypes.
 
 
 setwd("/file_location/")
 file_dir <- "/file_location/"
 file_prefix <- "input_file_prefix"
 records <- read.csv(paste0(file_dir, file_prefix,".csv"), header = T, stringsAsFactors = F)
-
-###Identification of D. chrysippus genotypes at the A, B and C loci###
-###Hindwing White (A-:absent,Aa:partial,aa:present)
-###Ground Colour (B-:present/Dark,Bb:intermediate,bb:absent/light)
-###Forewing Tip (C-:absent,Cc:intermediate,cc:present)
 
 #first turn all NA vlalues to zero, so we can add without problems
 for (trait in c("aa", "Aa", "A.", "bb", "B.", "cc", "Cc", "C.")){
