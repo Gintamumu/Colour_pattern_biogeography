71307fb568ec6e0a261b51a332e681a751bcba84
Gintamumu <40757024+Gintamumu@users.noreply.github.com>
2022-02-15 15:35:44 +0000

    Update and rename Number_and_spelling_mistakes_check.r to 01_Number_and_spelling_mistakes_check.r

--- 01_Number_and_spelling_mistakes_check.r
+++ 01_Number_and_spelling_mistakes_check.r
@@ -1,5 +1,5 @@
 
-file_prefix <- "GBIF_Dchrysippus_20201206_20210721_20211101_2_2_5644_5467_citizen"
+file_prefix <- "your file"
 
 ###header=T: 1st line as header
 ###stringsAsFactors = F: not convert character strings into factors, which may make it easy to do such things as replace values.
@@ -30,7 +30,7 @@ butterflies$year[which(butterflies$year == "UNKNOWN")] <- NA
 ###The subset() function is the easiest way to select variables and observations. 
 ### |: Element-wise logical OR
 
-butterflies <- subset(butterflies, is.na(hindwingWhite)==FALSE | is.na(forewingTip)==FALSE | is.na(groundColour)==FALSE | is.na(sex)==FALSE | is.na(month)==FALSE | is.na(year)==FALSE)
+### butterflies <- subset(butterflies, is.na(hindwingWhite)==FALSE | is.na(forewingTip)==FALSE | is.na(groundColour)==FALSE | is.na(sex)==FALSE | is.na(month)==FALSE | is.na(year)==FALSE)
 
 ### or ignore the last subset() command to see how the numbers change.
 
@@ -50,25 +50,7 @@ table(butterflies$groundColour)
 ### quote=F: write a csv from R without quoted values
 write.csv(butterflies, paste0(file_prefix,".subset.csv"), row.names=F, quote=F)
 
-### Example website https://www.statmethods.net/graphs/bar.html###
-# Simple Bar Plot
-counts <- table(butterflies$month)
-barplot(counts, main="Month Distribution",
-   xlab="Number of Individuals")
 
-# Simple Horizontal Bar Plot with Added Labels
-counts <- table(butterflies$year)
-barplot(counts, main="Year Distribution", horiz=TRUE)
-
-# Simple Horizontal Bar Plot with Added Labels
-counts <- table(butterflies$sex)
-barplot(counts, main="Sex Distribution", horiz=TRUE)
-
-# Grouped Bar Plot
-counts <- table(butterflies$sex, butterflies$month)
-barplot(counts, main="Butterflies Distribution by Month and Sex",
-  xlab="Number of Month", col=c("darkblue","red"),
-  legend = rownames(counts), beside=TRUE)
 
 
 
